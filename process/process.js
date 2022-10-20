2271671472d9cbb3805dd504f99c2b66a3e5e162
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2022-10-20 14:52:02 +0530

    buttons link

--- process/process.js
+++ process/process.js
@@ -313,3 +313,37 @@ function myFunction() {
 }
 
 window.onscroll = function() {myFunction()};
+
+
+
+
+
+var magnets = document.querySelectorAll(".magnetic");
+var strength = 50;
+
+magnets.forEach((magnet) => {
+    magnet.addEventListener("mousemove", moveMagnet);
+    magnet.addEventListener("mouseout", function (event) {
+        TweenMax.to(event.currentTarget, 1, { x: 0, y: 0, ease: Power4.easeOut });
+    });
+});
+
+function moveMagnet(event) {
+    var magnetButton = event.currentTarget;
+    var bounding = magnetButton.getBoundingClientRect();
+
+    //console.log(magnetButton, bounding)
+
+    TweenMax.to(magnetButton, 1, {
+        x:
+            ((event.clientX - bounding.left) / magnetButton.offsetWidth - 0.5) *
+            strength,
+        y:
+            ((event.clientY - bounding.top) / magnetButton.offsetHeight - 0.5) *
+            strength,
+        ease: Power4.easeOut
+    });
+
+    //magnetButton.style.transform = 'translate(' + (((( event.clientX - bounding.left)/(magnetButton.offsetWidth))) - 0.5) * strength + 'px,'+ (((( event.clientY - bounding.top)/(magnetButton.offsetHeight))) - 0.5) * strength + 'px)';
+}
+
