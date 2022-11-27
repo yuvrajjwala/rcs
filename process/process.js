04aab4eb417e8d42b0d285cc4fc0a3c316db3247
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2022-11-28 03:22:18 +0530

    blob

--- process/process.js
+++ process/process.js
@@ -1,33 +1,67 @@
 //Background Animation...
+var width = window.innerWidth;
+if (width <= 900) {
+    document.getElementsByClassName('container')[0].innerHTML = `
 
 
-var ele = Math.floor(Math.random() * 5);
-var anim = Math.floor(Math.random() * 5);
-const root = document.querySelector(':root');
-const e = document.getElementById('_0' + ele);
-const e1 = document.getElementById('_00' + ele);
-const e2 = document.getElementById('_000' + ele);
-const e3 = document.getElementById('_0000' + ele);
-var es = getComputedStyle(root);
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="259.157" height="244.841" viewBox="0 0 259.157 244.841" id='blob'>
+  <defs>
+    <clipPath id="clip-path">
+      <rect id="Rectangle_2869" data-name="Rectangle 2869" width="228.867" height="211.644" fill="#fff"/>
+    </clipPath>
+    <linearGradient id="linear-gradient" x1="0.5" y1="0.216" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
+      <stop offset="0" stop-color="#c9f9b8"/>
+      <stop offset="1" stop-color="#62beb7"/>
+    </linearGradient>
+  </defs>
+  <g id="Group_10812" data-name="Group 10812" transform="translate(0 35.803) rotate(-9)">
+    <g id="Group_10811" data-name="Group 10811" clip-path="url(#clip-path)">
+      <path id="Path_5024" data-name="Path 5024" d="M92.927,5.958c23.893,8.582,42.951,27.03,59.033,42.6,14.431,13.968,15.977,18.806,29.517,28.795,18.606,13.727,27.887,13.592,37.388,27.367,2.495,3.617,12.716,18.445,8.964,34.506-1.239,5.305-4.642,13.459-19.241,23.8-18.384,13.019-32.915,14.208-52.256,19.514-49.479,13.573-55.966,33.659-82.866,27.843a61.916,61.916,0,0,1-31.485-18.324C32.91,182.13,31.06,173.337,23.4,147.789,7.1,93.424,2.129,89.187.441,65.212c-.216-3.069-2.033-28.9,3.717-41.883C14.481.021,59.845-5.926,92.927,5.958" transform="translate(0.109 0.119)" fill="url(#linear-gradient)"/>
+      <path id="Path_5025" data-name="Path 5025" d="M92.927,5.958c23.893,8.582,42.951,27.03,59.033,42.6,14.431,13.968,15.977,18.806,29.517,28.795,18.606,13.727,27.887,13.592,37.388,27.367,2.495,3.617,12.716,18.445,8.964,34.506-1.239,5.305-4.642,13.459-19.241,23.8-18.384,13.019-32.915,14.208-52.256,19.514-49.479,13.573-55.966,33.659-82.866,27.843a61.916,61.916,0,0,1-31.485-18.324C32.91,182.13,31.06,173.337,23.4,147.789,7.1,93.424,2.129,89.187.441,65.212c-.216-3.069-2.033-28.9,3.717-41.883C14.481.021,59.845-5.926,92.927,5.958Z" transform="translate(0.109 0.119)" fill="none"/>
+    </g>
+  </g>
+</svg>
 
-//variable changing for initial and final position 
-root.style.setProperty('--pos_x_init', Math.floor(Math.random() * (1000 - (-300) + 1) + (-300)) + 'px');
-root.style.setProperty('--pos_y_init', Math.floor(Math.random() * (900 - (950) + 1) + (950)) + 'px');
-root.style.setProperty('--pos_x_final', '-255px');
-root.style.setProperty('--pos_y_final', '-34px');
+      
+      `
+    document.getElementsByClassName('container')[0].style.display = 'block'
+    document.getElementById('blob').style.display = 'block'
+    document.getElementById('blob').style.marginTop = '-12%'
+    document.getElementById('blob').style.marginLeft = '-16%'
+    // document.getElementById('blob').style.scale = '4%'
+}
+else {
+    var ele = Math.floor(Math.random() * 5);
+    var anim = Math.floor(Math.random() * 5);
+    const root = document.querySelector(':root');
+    const e = document.getElementById('_0' + ele);
+    const e1 = document.getElementById('_00' + ele);
+    const e2 = document.getElementById('_000' + ele);
+    const e3 = document.getElementById('_0000' + ele);
+    var es = getComputedStyle(root);
 
-//animation addition
-e.setAttribute('style', 'display: block;position: relative;top:' + es.getPropertyValue('--pos_y_final') + ';left:' + es.getPropertyValue('--pos_x_final') + ';animation : anim0 1s ease-in-out; z-index:2;transform: scale(0.85);')
-e1.setAttribute('style', 'display: block ; position: absolute;top:3px;left:-280px;  height: 1000px;  width: 1000px; animation : anim1 2s ease-in-out ; rotate:-9deg;')
-e2.setAttribute('style', 'display: block ; position: absolute;top:20px;left:-300px;  height: 1000px;  width: 1000px; animation : anim2 2s ease-in-out ; rotate:-9deg;')
-e3.setAttribute('style', 'display: block ; position: absolute;top:30px;left:-330px;  height: 1000px;  width: 1000px;animation : anim3 2s ease-in-out ; rotate:-9deg;')
+    //variable changing for initial and final position 
+    root.style.setProperty('--pos_x_init', Math.floor(Math.random() * (1000 - (-300) + 1) + (-300)) + 'px');
+    root.style.setProperty('--pos_y_init', Math.floor(Math.random() * (900 - (950) + 1) + (950)) + 'px');
+    root.style.setProperty('--pos_x_final', '-255px');
+    root.style.setProperty('--pos_y_final', '-34px');
+
+    //animation addition
+    e.setAttribute('style', 'display: block;position: relative;top:' + es.getPropertyValue('--pos_y_final') + ';left:' + es.getPropertyValue('--pos_x_final') + ';animation : anim0 1s ease-in-out; z-index:2;transform: scale(0.85);')
+    e1.setAttribute('style', 'display: block ; position: absolute;top:3px;left:-280px;  height: 1000px;  width: 1000px; animation : anim1 2s ease-in-out ; rotate:-9deg;')
+    e2.setAttribute('style', 'display: block ; position: absolute;top:20px;left:-300px;  height: 1000px;  width: 1000px; animation : anim2 2s ease-in-out ; rotate:-9deg;')
+    e3.setAttribute('style', 'display: block ; position: absolute;top:30px;left:-330px;  height: 1000px;  width: 1000px;animation : anim3 2s ease-in-out ; rotate:-9deg;')
 
 
-setTimeout(() => {
-    e1.style.transform = 'scale(0.9)';
-    e2.style.transform = 'scale(1.15)';
-    e3.style.transform = 'scale(1.35)';
-}, 2000);
+    setTimeout(() => {
+        e1.style.transform = 'scale(0.9)';
+        e2.style.transform = 'scale(1.15)';
+        e3.style.transform = 'scale(1.35)';
+    }, 2000);
+
+}
+
+
 
 
 
--- services/services.js
+++ services/services.js
@@ -1,34 +1,70 @@
 
 //Background Animation...
+var width = window.innerWidth;
+if(width<=900){
+    document.getElementsByClassName('container')[0].innerHTML = `
 
 
-var ele = Math.floor(Math.random() * 5);
-var anim = Math.floor(Math.random() * 5);
-const root = document.querySelector(':root');
-const e = document.getElementById('_0' + ele);
-const e1 = document.getElementById('_00' + ele);
-const e2 = document.getElementById('_000' + ele);
-const e3 = document.getElementById('_0000' + ele);
-var es = getComputedStyle(root);
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="317.29" height="313.939" viewBox="0 0 317.29 313.939" id='blob'>
+        <defs>
+          <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
+            <stop offset="0" stop-color="#fff"/>
+            <stop offset="1" stop-color="gray"/>
+          </linearGradient>
+          <clipPath id="clip-path">
+            <rect id="Rectangle_2849" data-name="Rectangle 2849" width="317.29" height="313.939" fill="url(#linear-gradient)"/>
+          </clipPath>
+          <linearGradient id="linear-gradient-2" x1="0.5" y1="0.331" x2="0.5" y2="1.169" gradientUnits="objectBoundingBox">
+            <stop offset="0" stop-color="#c9f9b8"/>
+            <stop offset="1" stop-color="#62be90"/>
+          </linearGradient>
+        </defs>
+        <g id="Group_10682" data-name="Group 10682" opacity="0.998">
+          <g id="Group_10681" data-name="Group 10681" transform="translate(0 0)" clip-path="url(#clip-path)">
+            <path id="Path_4955" data-name="Path 4955" d="M129.039,50.651C108.821,75.019,117.573,86.889,92.907,111.6,66.743,137.819,47.1,134.278,21.9,165.015,11.773,177.369-.249,192.473.536,213.086c1.007,26.421,22.357,43.538,25.763,46.185,22.4,17.406,39.707,8.054,78.547,15.081,63.514,11.491,75.925,47.358,114.364,38.331,27.911-6.555,43.224-30.436,44.615-32.675,15.752-25.371,2.086-42.231,10.054-79.489,9.562-44.713,34.479-44.91,41.787-82,7.1-36.019-6.645-85.335-41.787-106.824-46.743-28.583-114.5,2.387-144.84,38.959" transform="translate(-0.343 -0.343)" fill="url(#linear-gradient-2)"/>
+            <path id="Path_4956" data-name="Path 4956" d="M129.039,50.651C108.821,75.019,117.573,86.889,92.907,111.6,66.743,137.819,47.1,134.278,21.9,165.015,11.773,177.369-.249,192.473.536,213.086c1.007,26.421,22.357,43.538,25.763,46.185,22.4,17.406,39.707,8.054,78.547,15.081,63.514,11.491,75.925,47.358,114.364,38.331,27.911-6.555,43.224-30.436,44.615-32.675,15.752-25.371,2.086-42.231,10.054-79.489,9.562-44.713,34.479-44.91,41.787-82,7.1-36.019-6.645-85.335-41.787-106.824C227.135-16.891,159.381,14.079,129.039,50.651Z" transform="translate(-0.343 -0.343)" fill="none"/>
+          </g>
+        </g>
+      </svg>
+      
+      `
+      document.getElementsByClassName('container')[0].style.display = 'block'
+    document.getElementById('blob').style.display = 'block'
+    document.getElementById('blob').style.marginTop = '-5%'
+    document.getElementById('blob').style.marginLeft = '34%'
+    // document.getElementById('blob').style.scale = '4%'
+}
+else{
+    console.log('yesssssssssssssssssssssss')
+    var ele = Math.floor(Math.random() * 5);
+    var anim = Math.floor(Math.random() * 5);
+    const root = document.querySelector(':root');
+    const e = document.getElementById('_0' + ele);
+    const e1 = document.getElementById('_00' + ele);
+    const e2 = document.getElementById('_000' + ele);
+    const e3 = document.getElementById('_0000' + ele);
+    var es = getComputedStyle(root);
+    
+    //variable changing for initial and final position 
+    root.style.setProperty('--pos_x_init', Math.floor(Math.random() * (1000 - (-300) + 1) + (-300)) + 'px');
+    root.style.setProperty('--pos_y_init', Math.floor(Math.random() * (900 - (950) + 1) + (950)) + 'px');
+    root.style.setProperty('--pos_x_final', 'calc(50vw - 420px)');
+    root.style.setProperty('--pos_y_final', '-300px');
+    
+    //animation addition
+    e.setAttribute('style', 'display: block;position: relative;top:' + es.getPropertyValue('--pos_y_final') + ';left:' + es.getPropertyValue('--pos_x_final') + ';animation : anim0 1s ease-in-out; z-index:2;transform: scale(0.85);')
+    e1.setAttribute('style', 'display: block ; position: absolute;top:-450px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px; animation : anim1 2s ease-in-out ;')
+    e2.setAttribute('style', 'display: block ; position: absolute;top:-450px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px; animation : anim2 2s ease-in-out ;')
+    e3.setAttribute('style', 'display: block ; position: absolute;top:-500px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px;animation : anim3 2s ease-in-out ;')
+    
+    
+    setTimeout(() => {
+        e1.style.transform = 'scale(0.9)';
+        e2.style.transform = 'scale(1)';
+        e3.style.transform = 'scale(1.25)';
+    }, 2000);
 
-//variable changing for initial and final position 
-root.style.setProperty('--pos_x_init', Math.floor(Math.random() * (1000 - (-300) + 1) + (-300)) + 'px');
-root.style.setProperty('--pos_y_init', Math.floor(Math.random() * (900 - (950) + 1) + (950)) + 'px');
-root.style.setProperty('--pos_x_final', 'calc(50vw - 420px)');
-root.style.setProperty('--pos_y_final', '-300px');
-
-//animation addition
-e.setAttribute('style', 'display: block;position: relative;top:' + es.getPropertyValue('--pos_y_final') + ';left:' + es.getPropertyValue('--pos_x_final') + ';animation : anim0 1s ease-in-out; z-index:2;transform: scale(0.85);')
-e1.setAttribute('style', 'display: block ; position: absolute;top:-450px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px; animation : anim1 2s ease-in-out ;')
-e2.setAttribute('style', 'display: block ; position: absolute;top:-450px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px; animation : anim2 2s ease-in-out ;')
-e3.setAttribute('style', 'display: block ; position: absolute;top:-500px;left:calc(50vw - 500px);  height: 1000px;  width: 1000px;animation : anim3 2s ease-in-out ;')
-
-
-setTimeout(() => {
-    e1.style.transform = 'scale(0.9)';
-    e2.style.transform = 'scale(1)';
-    e3.style.transform = 'scale(1.25)';
-}, 2000);
+}
 
 
 
