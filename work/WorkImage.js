0730d32f1f7600f0fbfcfa42a680cbec79bf7b7e
yuvrajjwala <yuvrajjwala@gmail.com>
2023-11-04 13:21:20 +0530

    feat: adding work page

---
+++ work/WorkImage.js
@@ -0,0 +1,82 @@
+function workImage(name){
+    
+    const all = [
+        {
+            "name": "Nimble",
+            "desc": "An innovative application that connects homeowners with reliable and professional contractors who provide construction, housekeeping services, house remodeling, and house repair services."
+        },
+        {
+            "name": "Woodstock",
+            "desc": "A thesis and research-driven fund, investing in early-stage Distributed Ledger. Woodstock invests in early and growth-stage Distributed Ledger Technology (DLT) startups and companies."
+        },
+        {
+            "name": "9Media Online",
+            "desc": "Headquartered in Singapore, 9media provides an end-to-end solution designed for Connected TV advertising."
+        },
+        {
+            "name": "India Data Hub",
+            "desc": "Providing a range of analytical tools specifically designed to enable users to understand the data. The visualization engine allows users to visualize data, compare various indicators, transform data, and perform basic statistical analysis."
+        },
+        {
+            "name": "Needefy",
+            "desc": "A consulting company with significant expertise and experience within IT, Telecom, and Industry. Our team of experts analyze your needs and propose customer-specific solutions."
+        },
+        {
+            "name": "Drip Sports",
+            "desc": "A comprehensive solution that simplifies the process of managing sports leagues, programs, and camps. It aims to streamline communication between coaches, parents, and players using a dynamic scheduling system."
+        },
+        {
+            "name": "Green Laces",
+            "desc": "The leading retailer in the Nordics for vegan shoes and accessories that have been ethically and sustainably produced since 2010."
+        },
+        {
+            "name": "Lenovo",
+            "desc": "A multinational technology company renowned for its diverse range of computers, smartphones, and electronic devices."
+        },
+        {
+            "name": "Branova",
+            "desc": "Innovative and energy-efficient air purification solutions have been designed for you. Decades worth of data, skill and tech guarantee our expertise in clean-air solutions at a global scale."
+        },
+        {
+            "name": "Galacterium",
+            "desc": "The Galacterium Omniverse offers a suite of Utility tokens."
+        },
+        {
+            "name": "Macron",
+            "desc": "Producing hard to create cutting edge, high performance sportswear to support athletes from grassroots to professionals in their efforts to become their own hero on and off the field."
+        },
+        {
+            "name": "evAI",
+            "desc": "evAI Intelligence is an AI-based company and solution provider in the contextual intelligence market."
+        },
+        {
+            "name": "Lararnas a-kassa",
+            "desc": "Sweden’s largest unemployment benefit fund for teachers and educators specializing in teachers’ working conditions and help make overall improvements to the unemployment insurance fund."
+        },
+        {
+            "name": "Veito",
+            "desc": "Veito's sleek infrared heaters, built with high IP ratings and carbon fiber technology, offer sun-like warmth, remote control, and various power modes, available through Nordic and Baltic country dealers."
+        },
+        {
+            "name": "Photonet",
+            "desc": "Cutting-edge AI-driven facial recognition technology offers precise face scanning upon uploading a selfie, displaying relevant photos containing the user."
+        },
+        {
+            "name": "Ecoklimat",
+            "desc": "Addressing property owners who want to save energy in a qualitative and environmentally friendly way through their choice of facilities for indoor climate."
+        },
+        {
+            "name": "Xenon",
+            "desc": "Empowering Global Adoption by Offering Comprehensive Blockchain Solutions that Enhance Accessibility and Efficiency Worldwide."
+        },
+        {
+            "name": "Zood",
+            "desc": "Elevating global real estate prominence by innovatively establishing modern, multi-functional, and high-quality residential and commercial projects, adhering to the highest international standards."
+        }
+    ]
+
+
+
+
+
+}
